a5bb5d240e051b5841260ba8a3bf29abfb34b3b6
leon@scaledimages.com <leon@scaledimages.com>
2022-09-25 12:01:34 -0700

    updated README and core code

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # port2land
 
-## Currently A Work In Progress
+## Portrait to Landscape
+
+### Currently A Work In Progress
 
 Utility to format portrait photos onto a landscape background using the ImageMagick library
 using the the original photo as a stretched/dimmed background image to fill the
@@ -9,6 +11,14 @@ landscape frame.
 Currently a command line application with some fixed options.  Eventually it
 will also include command line options to teak some settings along with a GUI 
 to select and view the files.
+
+### Design Notes
+
+ * Makefile based builds (may switch to xmake)
+ * imagemagick library for all image manipulation
+ * uses fmt c++ formatting library to help format imagemagick options
+ * since this is more than likely going to be used to post images
+   online or publicly, all exif data is stripped out
 
 ### Background
 
@@ -20,6 +30,15 @@ that were done in portrait mode, and on some of them the frame would not rotate 
 unless it was on a landscape background.  There were too many to justify doing them
 one by one in Gimp, and while I was at it I thought I'd code it so that it created the
 stretched blurry background like you see on TV/TicTok.
+
+Here is an example of the original image:
+
+![port2land before image](https://assets.scaledimages.com/img/port2land-before.jpg "Original Image")
+
+
+Here is what it looks like after running through port2land
+
+![port2land aftert image](https://assets.scaledimages.com/img/port2land-after.jpg "Output Image")
 
 ### Current Status
 
@@ -41,3 +60,9 @@ output file name hard coded.
    - choose output filename
  * Port to OSX and Windows
  * Create Binary Installation Packages
+
+### Support This Project
+
+If you wind up using this or otherwise find it helpful feel free to:
+
+[![buy me a coffee](https://assets.scaledimages.com/img/buy-me-a-coffee.png "Buy Me A Coffee")](https://www.buymeacoffee.com/scaledimages)
--- port2land.cpp
+++ port2land.cpp
@@ -14,7 +14,7 @@ int main (int argc , char **argv ) {
     string outputFileName = "";
 
     int outHeight = 1024;
-    int outWidth = 1280;
+    int outWidth = 1280; 
 
     int origHeight = 0;
     int origWidth = 0;
@@ -22,10 +22,11 @@ int main (int argc , char **argv ) {
     int leftOffset = 0;
     int rightOffset = 0; 
 
+    // imagemagick Images
     Image origImage;
-
     Image outputImage;
 
+    // check command line arguments
     if ( argc != 2 ) {
         cerr << "Usage: " << argv[0] << " inputfile " << endl;
         exit(-1);
@@ -35,8 +36,7 @@ int main (int argc , char **argv ) {
         origWidth = origImage.columns();
     } 
 
-    cout << "Input: " << inputFileName << endl;
-
+    // rotate image if wider than taller
     origImage.read(inputFileName);
     if ( origImage.columns() > origImage.rows() ) { 
         origImage.rotate(-90);
@@ -44,29 +44,36 @@ int main (int argc , char **argv ) {
 
     // create destination image
     outputImage = origImage;
+    string resizeStr = fmt::format("{}x{}!",outWidth,outHeight);
+    outputImage.resize(resizeStr);
 
-    outputImage.resize("1280x1024!");
-    
+    // blur and dim the background
     outputImage.blur(0.0,6.0);
-    // outputImage.opacity(100);
     outputImage.modulate(55.0,100.0,100.0);
 
-
-    // set the new image to the same height as the output image
-    origImage.resize("x1020");
+    // resize the new image to the same height as the output image,
+    // and add a grey 2 pixel border
+    resizeStr = fmt::format("x{}",outHeight);
+    origImage.resize(resizeStr);
     origImage.borderColor("grey");
     origImage.border("2x2-2-2");
     
-    // figure out x offset
+    // figure out x offset to center horizontally on background
     leftOffset = ( outputImage.columns() - origImage.columns() ) / 2;
-      
     string compositeGeometry = fmt::format("+{}+0",leftOffset); //"+" + leftOffset + "+0";
-
     outputImage.composite( origImage , compositeGeometry , OverCompositeOp );
 
-    // set orientation exif tag appropriately
+    // strip all exif data
+    outputImage.strip();
 
-    outputImage.write("testme.jpg");
+    // set image orientation
+    //  - maybe later link to exiv2 library and set some
+    //    exif tags
+    outputImage.orientation(TopLeftOrientation);
+
+    // save the image
+    outputImage.quality(90);
+    outputImage.write("output.jpg");
 
     return 0;
 }
